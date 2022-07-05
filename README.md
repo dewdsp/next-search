e9a09c995844b027f31f6eab51b47b76a59e132f
Sitthakarn Prasomsup <sitthakarn.p@gmail.com>
2022-07-05 23:53:26 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
 # Next.js + Tailwind CSS Example
 
-This example shows how to use [Tailwind CSS](https://tailwindcss.com/) [(v3.0)](https://tailwindcss.com/blog/tailwindcss-v3) with Next.js. It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
+This example shows how to use [Search with debouce].
 
-## Deploy your own
+## Url for example
 
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
+Data in example [Interpol Notices API](https://interpol.api.bund.dev/)
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
+## Create Type for typescript
 
-## How to use
-
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
-
-```bash
-npx create-next-app --example with-tailwindcss with-tailwindcss-app
-# or
-yarn create next-app --example with-tailwindcss with-tailwindcss-app
-# or
-pnpm create next-app --example with-tailwindcss with-tailwindcss-app
-```
-
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+Create type using [app.quicktype.io](https://app.quicktype.io/)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,54 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import useDebounce from "../hooks/useDebounce";
+
+export interface Notice {
+  forename: string;
+  date_of_birth: string;
+  entity_id: string;
+  nationalities: string[];
+  name: string;
+  _links: Links;
+}
+
+export interface Links {
+  self: Images;
+  images: Images;
+  thumbnail: Images;
+}
+
+export interface Images {
+  href: string;
+}
 
 const Home: NextPage = () => {
+  const [notices, setNotices] = useState<Notice[]>([]);
+  const [search, setSearch] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    // search the api
+    async function fetchData() {
+      setLoading(true);
+
+      setNotices([]);
+
+      const data = await fetch(
+        `https://ws-public.interpol.int/notices/v1/red?forename=${debouncedSearch}&resultPerPage=200`
+      ).then((res) => res.json());
+      setNotices(data._embedded.notices);
+      setLoading(false);
+    }
+
+    if (debouncedSearch) fetchData();
+  }, [debouncedSearch]);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,75 +56,53 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
           </code>
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+        <input
+          className="mt-3 text-2xl outline-double"
+          type="search"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        {loading && <p>Loading...</p>}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {notices.map((notice) => {
+          return (
+            <div key={notice.entity_id} className="text-xl">
+              {notice._links.thumbnail?.href && (
+                <Image
+                  src={notice._links.thumbnail.href}
+                  width="100px"
+                  height="100px"
+                  alt={notice.name}
+                />
+              )}
+              <div className="flex mr-3">
+                <p>
+                  Name: {notice.forename} {notice.name}
+                </p>
+                <p className="text-blue-300 px-3">
+                  Birth: {notice.date_of_birth}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
